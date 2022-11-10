f5c4d262aad3d7bee5fbfd30e3f5a8210d37ef0f
Danae Komitopoulou <komitop.d@gmail.com>
2022-11-10 18:55:50 +0200

    Added css files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(stepOMeterRoutes);
 
 app.listen(3000);
